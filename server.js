11dce1b194dd214ed8967005e6f6a52359bc2a6d
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2024-04-02 01:04:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const corsConfig = {
-    origin: ['https://eco-eats-backend.vercel.app/', 'http://localhost:5173'], // Add 'http://localhost:5173' as an allowed origin
+    origin: ['https://eco-eats-backend.vercel.app', 'http://localhost:5173'], // Add 'http://localhost:5173' as an allowed origin
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
