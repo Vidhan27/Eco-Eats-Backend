1b0f1000a4fa99dc00c55414a6c4f43e74fa3186
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2024-04-03 11:04:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
     secret:'secret',
     store:new MongoStore({mongoUrl:process.env.MONGODB_URI}),
     resave:false,
-    secure: true, // set to false if not in production
+    secure: false, // set to false if not in production
     sameSite: 'none', // set to 'lax' if not in production
     saveUninitialized:false,
 }));
