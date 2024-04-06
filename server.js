3013bf5e698609d4dd3a82f7f51ae73c1ebf29f3
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2024-04-07 02:25:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
     secret:'secret',
     resave:false,
     saveUninitialized:true,
-    store: MongoStore({mongoUrl:process.env.MONGODB_URI,collectionName:"sessions"}),
+    store: new MongoStore({mongoUrl:process.env.MONGODB_URI,collectionName:"sessions"}),
     cookie:{
         maxAge:1000*60*60*24,
         secure:true,
