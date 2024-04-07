4370dd67db8eaf62db024f9d784083741305aa25
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2024-04-07 07:36:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(session({
     'cookie':{
         'maxAge':1000*60*60*24,
         'secure':false,
-        'httpOnly': false,
+        'httpOnly': true,
         'sameSite':'lax',
     }
     
