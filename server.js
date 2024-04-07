e8a7f0a9874e3993e2cacc7d28191ef1f194a6bc
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2024-04-07 08:06:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const corsConfig = {
 
-    origin: ['http://localhost:5173', 'https://eco-eats-backend.vercel.app'], // First two are frontend and Last one is for backend
+    origin: ['http://localhost:5173', 'https://eco-eats-backend.vercel.app','https://eco-eats-backend-vidhan-prajapatis-projects.vercel.app'], // First two are frontend and Last one is for backend
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
