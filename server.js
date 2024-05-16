ab1626846e011ef1d0ad9affaee39d4f6f7cd885
Vidhan27 <117517355+Vidhan27@users.noreply.github.com>
2024-05-16 21:01:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,12 +33,6 @@ app.use(session({
     secret:'keyboard-cat',
     resave:true,
     saveUninitialized:true,
-    store: new MongoStore({mongoUrl:process.env.MONGODB_URI,collectionName:"sessions"}),
-    cookie:{
-        maxAge:1000*60*60*24,
-        secure:true,
-        sameSite:'None',
-    }
     
 }));
 
